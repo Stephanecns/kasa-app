54c0acf2a2d0124ff6b6b061df8762668ea8d322
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2024-07-27 14:43:23 +0200

    Add : Ajout du fichier images

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+// src/components/Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="App-header">
+        
+      <nav>
+        <ul>
+          <li><Link to="/">Accueil</Link></li>
+          <li><Link to="/about">A Propos</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
