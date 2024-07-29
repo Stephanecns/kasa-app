7758e1b348f75fffebc50a2c09dcc06f3cd7ac4a
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2024-07-29 18:39:48 +0200

    Add : Ajout des dropdowns

---
+++ src/components/Dropdown.js
@@ -0,0 +1,25 @@
+// src/components/Dropdown.js
+import React, { useState } from 'react';
+import './Dropdown.scss';
+
+const Dropdown = ({ title, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="dropdown">
+      <button className="dropdown__title" onClick={toggleDropdown}>
+        {title}
+        <span className={`dropdown__arrow ${isOpen ? 'dropdown__arrow--open' : ''}`}>
+          &gt;
+        </span>
+      </button>
+      {isOpen && <div className="dropdown__content">{content}</div>}
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import Banner from '../components/Banner';
-import aboutBanner from '../images/Image-About.png';
+import React from "react";
+import Banner from "../components/Banner";
+import Dropdown from '../components/Dropdown';
+import aboutBanner from "../images/Image-About.png";
 
 const About = () => {
   return (
     <div>
       <Banner image={aboutBanner} alt="Bannière de la page d'accueil" />
-      <h1>Accueil</h1>
-      <p>Bienvenue sur notre site web !</p>
+      <Dropdown
+        title="Fiabilité"
+        content="La fiabilité est au cœur de notre engagement."
+      />
+      <Dropdown
+        title="Respect"
+        content="Le respect est une valeur fondamentale chez nous."
+      />
+      <Dropdown
+        title="Service"
+        content="Nous nous engageons à fournir un service de qualité."
+      />
+      <Dropdown
+        title="Sécurité"
+        content="La sécurité de nos utilisateurs est notre priorité."
+      />
     </div>
   );
 };
 
 export default About;
-
