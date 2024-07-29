5d4194042a59f69743024077d55b30517dc9490e
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2024-07-29 12:41:15 +0200

    Add : Ajout du font + config header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 // Composant principal de l'application. Définit les routes et la structure de navigation.
+import React from 'react';
+import Header from './components/Header';
+import './styles/main.scss';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
 
 function App() {
   return (
     <div className="App">
-
+    <Header />
+    <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 // src/components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/LOGO-DH.png';
 import './Header.scss';
 
 const Header = () => {
   return (
-    <header className="App-header">
-        
-      <nav>
-        <ul>
-          <li><Link to="/">Accueil</Link></li>
-          <li><Link to="/about">A Propos</Link></li>
+    <header className="header">
+      <div className="header__logo-container">
+        <img src={logo} alt="Logo" className="header__logo" />
+      </div>
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          <li className="header__nav-item"><Link to="/" className="header__nav-link">Accueil</Link></li>
+          <li className="header__nav-item"><Link to="/about" className="header__nav-link">A Propos</Link></li>
         </ul>
       </nav>
     </header>
