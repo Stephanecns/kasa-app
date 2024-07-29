dd2a81b421a99ebce0d85f9c194c78c75ddc71f4
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2024-07-29 12:40:58 +0200

    Add : Config compil fichier sass

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+// src/pages/About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>A Propos</h1>
+      <p>Nous sommes une entreprise innovante...</p>
+    </div>
+  );
+};
+
+export default About; // Exportation par défaut
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Accueil</h1>
+      <p>Bienvenue sur notre site web !</p>
+    </div>
+  );
+};
+
+export default Home; // Exportation par défaut
